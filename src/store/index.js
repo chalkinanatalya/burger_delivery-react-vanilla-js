9a0a98e1c9f67f64ea0abffc76fa0615acac16e0
Natalya <m_d.r.lecter@protonmail.com>
2023-03-15 22:00:11 +0400

    modal window

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice";
 import productReducer from "./product/productSlice";
 import orderReucer, { localStorageMiddleware } from "./order/orderSlice";
+import modalReducer from './modalDelivery/modalDeliverySlice';
 
 export const store = configureStore({
     //reads present object state, returns new state
@@ -9,6 +10,7 @@ export const store = configureStore({
         category: categoryReducer,
         product: productReducer,
         order: orderReucer,
+        modal: modalReducer,
     },
 
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(localStorageMiddleware),
